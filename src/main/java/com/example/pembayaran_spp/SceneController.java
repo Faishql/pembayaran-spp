5c81861a6f8c45075cef7a0ba1a6ada915288e73
Faishql <68933048+Faishql@users.noreply.github.com>
2022-01-23 15:50:16 +0700

    u f

--- src/main/java/com/example/pembayaran_spp/SceneController.java
+++ src/main/java/com/example/pembayaran_spp/SceneController.java
@@ -18,13 +18,13 @@ public class SceneController {
  public void switchToScene1(ActionEvent event) throws IOException {
   root = FXMLLoader.load(getClass().getResource("login.fxml"));
   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-  scene = new Scene(root);
+  scene = new Scene(root, 900, 600);
   stage.setScene(scene);
   stage.show();
  }
  
  public void switchToScene2(ActionEvent event) throws IOException {
-  Parent root = FXMLLoader.load(getClass().getResource("dataSiswa.fxml"));
+  Parent root = FXMLLoader.load(getClass().getResource("halDataSiswa.fxml"));
   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
   scene = new Scene(root, 1000, 600);
   stage.setScene(scene);
